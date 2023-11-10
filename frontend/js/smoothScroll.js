a842ce9243b8c304fa2c580f0e30ab27cc6ab0f7
지수진 <zi_soo_zin@naver.com>
2023-11-10 23:31:43 +0900

    [변경] autoSlide.js 코드 smoothScroll.js로 이동

--- frontend/js/smoothScroll.js
+++ frontend/js/smoothScroll.js
@@ -2,52 +2,52 @@
 window.addEventListener("DOMContentLoaded", scrollFn);
 
 function scrollFn() {
-
     // 휠 이벤트로 페이지 이동 컨트롤 ///////////////
     const indic = document.querySelectorAll(".main_nav a");
 
     // 인디케이터 메뉴 이벤트 연결
-    indic.forEach((ele,idx,obj)=>{
-        ele.addEventListener("click", ()=>movePg(idx,obj));
-    })
+    indic.forEach((ele, idx, obj) => {
+        ele.addEventListener("click", () => movePg(idx, obj));
+    });
 
-    // 0. 변수설정
-    // (1) 전체 페이지 변수 
+    // 페이지 변수
     let pgnum = 0;
-    // (2) 전체 페이지 수
-    const pg =  document.querySelectorAll(".page");
+    // 페이지 수
+    const pg = document.querySelectorAll(".page");
     const pgcnt = pg.length;
-    // (3) 광스크롤 금지변수 0-허용 1-불허용
+    // 광스크롤 금지변수 0-허용 1-불허용
     let prot_sc = 0;
 
     // 1. 전체 휠 이벤트 설정 /////
-    window.addEventListener("wheel", wheelFn, {passive:false})
+    window.addEventListener("wheel", wheelFn, { passive: false });
+
+    autoSlide();
 
     function wheelFn(e) {
         // 기본기능 막기
         e.preventDefault();
 
         // 광스크롤 막기
-        if(prot_sc) return;
+        if (prot_sc) return;
         prot_sc = 1;
-        setTimeout(()=>prot_sc=0,600);
+        setTimeout(() => (prot_sc = 0), 600);
 
         // 휠 방향 알아내기
         let dir = e.wheelDelta;
 
         // 방향에 따른 페이지 번호 증가
         // 스크롤 아랫방향 : 페이지번호 증가
-        if(dir<0) {
+        if (dir < 0) {
             // 페이지번호 1씩 증가
             pgnum++;
             // 한계수 : 페이지 끝번호(페이지수 - 1)
-            if(pgnum>pgcnt-1) pgnum = pgcnt-1
+            if (pgnum > pgcnt - 1) pgnum = pgcnt - 1;
         }
         // 스크롤 윗방향 : 페이지번호 감소
         else {
             pgnum--;
             // 한계수 : 페이지 첫번호(페이지수 - 0)
-            if(pgnum<0) pgnum = 0
+            if (pgnum < 0) pgnum = 0;
         }
 
         updatePg(indic);
@@ -58,45 +58,92 @@ function scrollFn() {
         // 기본기능막기
         event.preventDefault();
         //페이지번호 업데이트
-        pgnum = seq
+        pgnum = seq;
         // 함수호출
         updatePg(indic);
     }
-    
-    /// 페이지 이동 설정값 업데이트
+
+    // 페이지 이동 설정값 업데이트
     async function updatePg(obj) {
         try {
             // 1. 페이지 이동
-            window.scrollTo(0, window.innerHeight*pgnum);
-    
+            window.scrollTo(0, window.innerHeight * pgnum);
+
             // 메뉴초기화 (class on제거)
-            for(let x of obj) {
+            for (let x of obj) {
                 x.parentElement.classList.remove("on");
             }
-            
+
             // 해당 메뉴에 클래스 넣기
             obj[pgnum].parentElement.classList.add("on");
-    
+
             chgColor(pgnum);
-        }
-        catch (error) {
-            console.log("푸터!")
+        } catch (error) {
+            console.log("푸터!");
         }
     }
 
-    // 2,3,4,5번페이지 gnb 및 인디케이터, 컨택트 아이콘 색상 다르게 하기 
-    function chgColor (c) {
+    // 자동+페이드 슬라이드
+    function autoSlide() {
+        // 대상선정 함수
+        const qs = x => document.querySelector(x);
+        const qsa = x => document.querySelectorAll(x);
+
+        // 클래스 추가/제거함수
+        const addCls = (x, y) => x.classList.add(y);
+        const removeCls = (x, y) => x.classList.remove(y);
+
+        // 변경 대상 1 : 슬라이드 리스트
+        const slide = qsa(".main_content_wrapper li");
+        // 변경 대상 2 : 타임바
+        const rtimeBar = qs(".main_timebar");
+        // 현재 슬라이드번호
+        const ctnum = qs(".ctnum");
+
+        // 인터벌 함수 지우기 위한 변수
+        let autoI;
+        // 타임아웃 함수 지우기 위한 변수
+        let autoT;
+
+        // 슬라이드번호 변수
+        let snum = 0;
+        // 슬라이드 개수
+        let slideLength = slide.length;
+
+        const fadeSlide = () => {
+            // 클래스 초기화
+            for (let x of slide) removeCls(x, "on");
+            // 클래스 추가
+            addCls(slide[snum], "on");
+
+            // 슬라이드 번호 증가
+            autoT = setTimeout(() => {
+                snum++;
+                // 한계값 체크
+                if (snum === slideLength) snum = 0;
+            }, 1000);
+
+            addCls(rtimeBar, "on");
+            // 현재 슬라이드 번호 표시
+            ctnum.innerText = snum + 1;
+        };
+
+        fadeSlide();
+        autoI = setInterval(fadeSlide, 5000);
+    }
+    
+    // 2,3,4,5번페이지 gnb 및 인디케이터, 컨택트 아이콘 색상 다르게 하기
+    function chgColor(c) {
         // 1. 대상선정
-        // 인디케이터 
+        // 인디케이터
         const ind = document.querySelector(".main_nav");
 
         // 2. 클래스 넣기
-        if(c===1||c===2||c===3||c===4) {
-            ind.classList.add("on"); 
+        if (c === 1 || c === 2 || c === 3 || c === 4) {
+            ind.classList.add("on");
         }
         else {
             ind.classList.remove("on");
         }
-
     } // chgColor 함수
 }
